2703546fb8985dc00d9e7ca4be1d8f266d73fbf4
Friederich <siyfred1n@gmail.com>
2025-03-05 19:28:46 -0300

    add: adicionarAmigo function

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+console.log(amigos);
+
+function adicionarAmigo() {
+    const nome = document.getElementById("amigo").value.trim();
+    if(nome == "") {
+        alert("O campo 'Digite um nome' não pode ser vazio");
+        return;
+    }
+    if(nome.includes(["0","1","2","3","4","5","6","7","8","9"])) {
+        alert("O campo 'Digite um nome' não pode conter números");
+        return;
+    }
+    if(amigos.includes(nome)) {
+        alert("Um amigo com esse nome já foi adicionado");
+        return;
+    }
+    amigos.push(nome);
+    console.log(amigos);
+}
+
